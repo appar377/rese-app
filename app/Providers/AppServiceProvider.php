e9c3697a50cf339610b10bcc9fb04ffc44b2a81b
appar377 <riku.ren.sou@gmail.com>
2022-07-05 20:21:39 +0900

    コミット

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Schema::defaultStringLength(191);
     }
 }
