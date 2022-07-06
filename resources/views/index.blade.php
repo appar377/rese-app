41caf634c3fd7226984ef77acf19330220b6dcab
appar377 <riku.ren.sou@gmail.com>
2022-07-06 23:04:25 +0900

    コミット

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('pageCss')
-<link rel="stylesheet" href="{{ mix('css/index.css') }}">
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
 @endsection
 
 @section('content')
