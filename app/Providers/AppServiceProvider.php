85c977917cecd5f7f74d5555782c3c8ac9bf6fd1
appar377 <riku.ren.sou@gmail.com>
2022-07-07 17:22:49 +0900

    コミット

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,10 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(env('FORCE_HTTPS',false)) {
-            URL::forceScheme('https');
-        }
-        
         Schema::defaultStringLength(191);
     }
 }
